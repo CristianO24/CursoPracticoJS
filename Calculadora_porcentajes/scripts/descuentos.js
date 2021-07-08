5f9083f11fd1eeffbdda26b8a4aa22d99873404c
CristianO24 <david941224@gmail.com>
2021-07-08 18:10:54 -0500

    Se añade logica e interfaz para manejo de cupones

--- Calculadora_porcentajes/scripts/descuentos.js
+++ Calculadora_porcentajes/scripts/descuentos.js
@@ -3,18 +3,61 @@ function CalcularPorcentaje(precio,porcentaje){
     return pfinal;
 }
 
-function CalcularResultado(){
-    const result = document.querySelector(".respuesta");
-    const Inputprecio = document.getElementById("Inputprecio");
-    const Inputdescuento = document.getElementById("Inputdescuento");
-    const valuep = Inputprecio.value;
-    const valued = Inputdescuento.value;
-    const valorf = CalcularPorcentaje(valuep,valued);
-    result.innerHTML = `El valor final a pagar es : ${valorf} $`;
+const adescuento = [
+  {
+    nombre: "holiday",
+    descuento: 15,
+  },
+  {
+    nombre: "halloween",
+    descuento: 20,
+  },{
+    nombre: "4july",
+    descuento: 30,
+  }
+];
+
+
+function Calcularcupon(cupon){
+
+  const isCouponValueValid = function (adescuento) {
+    return adescuento.nombre === cupon;
+  };
+
+const userCoupon = adescuento.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert("El cupón " + cupon + "no es válido");
+    const descuento = 0;
+    return descuento;
+
+  }else {
+    const descuento = userCoupon.descuento;
+    return descuento;
+  }
 }
 
-function maxLengthCheck(object)
-  {
-    if (object.value.length > object.maxLength)
-      object.value = object.value.slice(0, object.maxLength)
-  }
+
+function CalcularResultado(){
+  
+  //Traer los valores desde HTML
+  const Inputprecio = document.getElementById("Inputprecio");
+  const Inputdescuento = document.getElementById("Inputdescuento");
+  const Inputcupon = document.getElementById("Inputcupon");
+  const valuep = parseInt(Inputprecio.value);
+  let valued = parseInt(Inputdescuento.value);
+  const valuec = Inputcupon.value;
+
+  //Ejecuta la funciones para saber si aplica un cupon y calcula el descuento
+  const valorc = Calcularcupon(valuec);   
+  valued = valued + valorc;
+  const valorf = CalcularPorcentaje(valuep,valued);
+  
+  //Respuestas 
+  const result = document.querySelector(".respuesta");
+  const descupon = document.querySelector(".descupon")
+  result.innerHTML = `El valor final a pagar es : ${valorf} $`;
+  descupon.innerHTML = `El porcentaje de descuento del cupon es de ${valorc}%`;
+  
+
+}
