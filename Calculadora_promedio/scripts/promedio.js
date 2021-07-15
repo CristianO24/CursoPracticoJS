29476408538ec1b9258e555a5bda8b01616af521
CristianO24 <david941224@gmail.com>
2021-07-14 21:44:24 -0500

    Se agrega logica del promedio y estilos

--- Calculadora_promedio/scripts/promedio.js
+++ Calculadora_promedio/scripts/promedio.js
@@ -1,54 +1,58 @@
-function CalculaPromedio(lista){
-    lista = new Array();
-    let total = 0;
+let lista = new Array();
 
-    for (let i = 0; i < lista.length; i++){
-        total = total + lista[i];
-    }
-    
-    const promedio = (total/lista.length)
-
-    console.log(total);
-    console.log(promedio);
-}
-
-/*function MostrarPromedio(){
-    const result = document.querySelector(".promedio_respuesta"); 
-    const lista = document.getElementById("InputPromedio");
-    const valorLista = [].map.call(lista,function(){
-
-    }
-    const resultado = CalculaPromedio(valorLista);
-    result.innerHTML = `El promedio es ${resultado}`;    
-
-}*/
-
+//capturar el numero de datos
 function numeroDatos(){
     let valor = document.getElementById("InputDatos");
     let numero = parseInt(valor.value);
     return numero;
 }
 
-
+//Insertar la cantidad de inputs indicados
 function addInput () {
     //eliminar todos los elementos hijos del padre;
     var elemento = document.getElementById("addInputs");
     while (elemento.firstChild) {
       elemento.removeChild(elemento.firstChild);
     };
-    //quitar respuesta
-    /*const resultado = document.getElementById("promedio_respuesta");
-    resultado.innerHTML = " ";*/
-
     //colocar los imput
     let valorNumeroDatos =  numeroDatos();
 
-   for(let i = 1; i < (valorNumeroDatos + 1); i++ ) {
-        
-        var direccion = 
-        `<input id="${i}" type="number" placeholder = "Dato # ${i}"></input>`;
+   for(let i = 1; i < (valorNumeroDatos + 1); i++ ) {      
+        var inputs = `<input id="${i}" type="number" placeholder = "Dato # ${i}"></input>`;
         let input = document.getElementById("addInputs");
-        input.insertAdjacentHTML('beforeend', direccion);
+        input.insertAdjacentHTML('beforeend', inputs);
+    }     
+}
+
+//capturas los datos e ingresarlos en el array
+function datosUsuario () {
+    let valorNumeroDatos =  numeroDatos ();
+
+    //trae los datos a js y los agrega a la
+    for (let i = 1; i < (valorNumeroDatos + 1); i++){
+        let id = i;
+        let dato = document.getElementById(id);
+        let valueDato = parseInt(dato.value);
+        lista.push(valueDato);
+    }
+}
+
+//Calcular promedio con el array lleno
+function CalculaPromedio(){
+    lista = [];
+    datosUsuario();
+    let total = 0;
+
+    for (let i = 0; i < lista.length; i++){
+        total = total + lista[i];
     }
     
+    const promedio = (total/lista.length);    
+    return promedio;
+}
+
+function RespuestaPromedio(){
+   const result = document.querySelector(".promedio_respuesta");
+   const promedio = CalculaPromedio();
+   result.innerHTML = `El promedio es : ${promedio}`;
 }
